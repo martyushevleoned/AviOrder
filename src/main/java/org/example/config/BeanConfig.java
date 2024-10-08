4a4269f5aef253effb38b0ec7f03db6a50c169f4
Martyushev Leoned <lyonya.martyushev@mail.ru>
2024-10-08 19:38:48 +0500

    redirect http to https

--- src/main/java/org/example/config/BeanConfig.java
+++ src/main/java/org/example/config/BeanConfig.java
@@ -1,5 +1,8 @@
 package org.example.config;
 
+import org.apache.catalina.connector.Connector;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,7 +12,19 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class BeanConfig {
 
     @Bean
-    public PasswordEncoder getPasswordEncoder() {
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(8);
+    }
+
+    @Bean
+    public ServletWebServerFactory servletWebServerFactory() {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        connector.setRedirectPort(8443);
+        connector.setSecure(false);
+        connector.setScheme("http");
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
     }
 }
